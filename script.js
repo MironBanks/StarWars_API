8b808af62b0dd89d7823bbacea4b663235ec56e3
Miron Banks <madapegarage@gmail.com>
2020-02-07 05:49:53 +0100

    fetch and css films cards

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const results = document.querySelector('#results');
 
-function asyncFetch(value) {
-	console.log(value);
+async function asyncFetch(value) {
+	const res = await fetch(`https://swapi.co/api/${value}/`);
+	const data = await res.json();
+	displayResults(data, value);
+}
+
+function displayResults(data, value) {
+	let output = '';
+	if (value === 'films') {
+		data.results.forEach(item => {
+			output += `
+            <div class="card"> 
+                <h4 class="card_title">${item.title}</h4>
+                <div class="card_content">
+                    <h5><span>Producer:</span> ${item.producer}</h5>
+                    <h5><span>Director:</span> ${item.director}</h5>
+                </div>
+            </div>
+
+            `;
+		});
+	}
+	results.innerHTML = output;
 }
 
 //event listener for buttons
